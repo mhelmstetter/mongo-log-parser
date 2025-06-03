c4036e343c65c0a02a6fb54b2bdf928073eccb15
Mark Helmstetter <mark@helmstetter.com>
2025-06-03 10:58:52 -0700

    Initial commit, needs work

---
+++ src/main/java/com/mongodb/logparse/AccumulatorKey.java
@@ -0,0 +1,78 @@
+package com.mongodb.logparse;
+
+import java.io.File;
+
+public class AccumulatorKey {
+    
+    private File file;
+    //private String namespace;
+    private String dbName;
+    private String collName;
+    private String command;
+    
+    public AccumulatorKey(File file, String dbName, String collName, String command) {
+        this.file = file;
+        this.dbName = dbName;
+        this.collName = collName;
+        this.command = command;
+    }
+    
+    public AccumulatorKey(File file, Namespace namespace, String command) {
+        this.file = file;
+        this.dbName = namespace.getDatabaseName();
+        this.collName = namespace.getCollectionName();
+        
+        this.command = command;
+    }
+    
+    public String toString() {
+        return hashCode()+"";
+    }
+    
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((collName == null) ? 0 : collName.hashCode());
+        result = prime * result + ((command == null) ? 0 : command.hashCode());
+        result = prime * result + ((dbName == null) ? 0 : dbName.hashCode());
+        result = prime * result + ((file == null) ? 0 : file.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        AccumulatorKey other = (AccumulatorKey) obj;
+        if (collName == null) {
+            if (other.collName != null)
+                return false;
+        } else if (!collName.equals(other.collName))
+            return false;
+        if (command == null) {
+            if (other.command != null)
+                return false;
+        } else if (!command.equals(other.command))
+            return false;
+        if (dbName == null) {
+            if (other.dbName != null)
+                return false;
+        } else if (!dbName.equals(other.dbName))
+            return false;
+        if (file == null) {
+            if (other.file != null)
+                return false;
+        } else if (!file.equals(other.file))
+            return false;
+        return true;
+    }
+    
+    
+
+}
---
+++ src/main/java/com/mongodb/logparse/LogLineAccumulator.java
@@ -0,0 +1,144 @@
+package com.mongodb.logparse;
+
+import java.io.File;
+
+import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
+
+public class LogLineAccumulator {
+    
+    private Namespace namespace;
+    private String operation;
+    private File file;
+    
+    private final static double ONE_MB_DOUBLE = 1024.0 * 1024.0;
+    private final static long ONE_MB = 1024 * 1024;
+    
+    private long count;
+    private long total;
+    private long min = Long.MAX_VALUE;
+    private long max = Long.MIN_VALUE;
+    
+    private long totalKeysExamined;
+    private long totalDocsExamined;
+    
+    private long totalReturned = 0;
+    
+    private long reslen = 0;
+    
+    private long bytesRead = 0;
+    
+    
+    DescriptiveStatistics executionStats = new DescriptiveStatistics();
+    
+    DescriptiveStatistics keysExaminedStats = new DescriptiveStatistics();
+    DescriptiveStatistics docsExaminedStats = new DescriptiveStatistics();
+    
+    
+    public LogLineAccumulator(File file, String command, Namespace namespace2) {
+        this.namespace = namespace2;
+        this.operation = command;
+        this.file = file;
+    }
+
+    public void addExecution(long amt) {
+        count++;
+        total += amt;
+        System.out.println("count: " + count + ", total: " + total);
+        if (amt > max) {
+            max = amt;
+        }
+        if (amt < min) {
+            min = amt;
+        }
+        //executionStats.addValue(amt);
+    }
+    
+    public double getKeysExaminedPercentile95() {
+        if (totalKeysExamined > 0) {
+            return keysExaminedStats.getPercentile(95);
+        }
+        return 0.0d;
+    }
+    
+    public double getDocsExaminedPercentile95() {
+        if (totalDocsExamined > 0) {
+            return docsExaminedStats.getPercentile(95);
+        }
+        return 0.0d;
+    }
+    
+    public double getPercentile95() {
+        return executionStats.getPercentile(95);
+    }
+    
+    public long getAvgDocsExamined() {
+        return totalDocsExamined/count;
+    }
+    
+    public long getTotalDocsExamined() {
+        return totalDocsExamined;
+    }
+    
+    public String toString() {
+        return String.format("%-65s %-20s %-10s %-10.1f %-10d %-10d %-10d %-10d %-10d %-10d %-12d %-10d %-10d", 
+        		namespace, operation, count, reslen/ONE_MB_DOUBLE, bytesRead/ONE_MB, min, max, total/count, total/1000,
+                totalKeysExamined/count, totalDocsExamined/count, 
+                //getKeysExaminedPercentile95(), getDocsExaminedPercentile95(), totalKeysExamined/1000.0, totalDocsExamined/1000.0, 
+                totalReturned/count, getScannedReturnRatio());
+        
+    }
+    
+    public String toCsvString() {
+        return String.format("%s,%s,%d,%d,%d,%10d,%10.0f,%10d,%10d,%10d,%10.0f,%10.0f,%12.1f,%12.1f,%10d,%10d", namespace, operation, count, 
+                min, max, total/count, getPercentile95(), total/1000,
+                totalKeysExamined/count, totalDocsExamined/count, 
+                getKeysExaminedPercentile95(), getDocsExaminedPercentile95(), 
+                totalKeysExamined/1000.0, totalDocsExamined/1000.0, totalReturned/count, getScannedReturnRatio());
+        
+    }
+    
+    
+
+    public long getScannedReturnRatio() {
+        if (totalReturned > 0) {
+            return totalDocsExamined/totalReturned;
+        }
+        return 0;
+    }
+
+    public void addExamined(Long keysExamined, Long docsExamined) {
+        totalDocsExamined += docsExamined;
+        totalKeysExamined += keysExamined;
+        //keysExaminedStats.addValue(keysExamined);
+        //docsExaminedStats.addValue(docsExamined);
+    }
+    
+    public void addReturned(Long nReturned) {
+        totalReturned += nReturned;
+    }
+    
+    public void addReslen(Long reslen) {
+        this.reslen += reslen;
+    }
+
+    public long getCount() {
+        return count;
+    }
+    
+    public long getMax() {
+        return max;
+    }
+
+    public long getAvg() {
+        return total/count;
+    }
+
+    public long getAvgReturned() {
+        return totalReturned/count;
+    }
+
+	public void addBytesRead(Long bytesRead) {
+		this.bytesRead += bytesRead; 
+	}
+
+}
